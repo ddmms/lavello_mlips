262cfa13a8d157b536562b04c5cc349b4feb19ef
alin m elena <alin@elena.re>
2026-03-22 06:38:45 +0000

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@ project = 'process_omol25'
 copyright = '2026, Alin M Elena'
 author = 'Alin M Elena'
 
-version = '0.1.1'
-release = '0.1.1'
+version = '0.1.2'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
